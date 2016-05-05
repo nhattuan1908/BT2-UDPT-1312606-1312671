9383f5f798d93ccc04a91f00875b506049c03709
Tran Tuan <Tran Tuan>
2016-05-05 15:28:30 +0700

    Commit on Github

---
+++ JS/source.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+        //  Focus auto-focus fields
+        $('.auto-focus:first').focus();
+        
+        //  Initialize auto-hint fields
+        $('INPUT.auto-hint, TEXTAREA.auto-hint').focus(function(){
+            if($(this).val() == $(this).attr('title')){ 
+                $(this).val('');
+                $(this).removeClass('auto-hint');
+            }
+        });
+        
+        $('INPUT.auto-hint, TEXTAREA.auto-hint').blur(function(){
+            if($(this).val() == '' && $(this).attr('title') != ''){ 
+                $(this).val($(this).attr('title'));
+                $(this).addClass('auto-hint'); 
+            }
+        });
+        
+        $('INPUT.auto-hint, TEXTAREA.auto-hint').each(function(){
+            if($(this).attr('title') == ''){ return; }
+            if($(this).val() == ''){ $(this).val($(this).attr('title')); }
+            else { $(this).removeClass('auto-hint'); } 
+        });
+ });
+
+
+$(function(){
+                var socket = io.connect();
+                var $messageForm = $('#messageForm');
+                var $message = $('#message');
+                var $chat = $('#chatWindow');
+                var $usernameForm = $('#usernameForm');
+                var $users = $('#users')
+                var $username = $('#username');
+                var $error = $('#error');
+
+                $usernameForm.submit(function(e){
+                    e.preventDefault();
+                    socket.emit('new user', $username.val(), function(data){
+                        if(data){
+                            $('#namesWrapper').hide();
+                            $('#mainWrapper').show();
+                        } else {
+                            $error.html('Username is already taken');
+                        }
+                    });
+                    $username.val('');
+                });
+
+                socket.on('usernames', function(data){
+                    var html = '';
+                    for(i = 0; i < data.length; i++){
+                        html += data[i] + '<br/>';
+                    }
+                    $users.html(html);
+                })
+
+                $messageForm.submit(function(e){
+                    e.preventDefault();
+                    socket.emit('send message', $message.val());
+                    $message.val('');
+                });
+
+                socket.on('new message', function(data){
+                    $chat.append('<strong>'+ data.user + '</strong>: '+data.msg+'<br/>');
+                });
+            })
